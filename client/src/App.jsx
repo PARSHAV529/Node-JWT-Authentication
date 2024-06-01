0fdbc65736046dcaaf3e838dda9378c9432c71c9
Parshav <parshav143@gmail.com>
2024-06-01 20:07:48 +0530

    deploy commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import { Home } from './pages/Home';
 import Login from './pages/Login';
 import { RootLayout } from './pages/RootLayout';
 import SignUp from './pages/signUp'
+import { QueryClientProvider,QueryClient } from '@tanstack/react-query';
+
 import {
   createBrowserRouter,
   RouterProvider,
@@ -33,11 +35,13 @@ const router = createBrowserRouter([
 ])
 function App() {
   
+  const queryClient = new QueryClient()
 
   return (
     <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}/>
-
+      </QueryClientProvider>
     </>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,58 +1,73 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import { isExpired, decodeToken } from "react-jwt";
 import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
+
+async function getUserDetails() {
+  let config = {
+    headers: {
+      'x-access-token': localStorage.getItem('token'),
+    }
+  }
+
+  const res = await axios.get("https://node-jwt-authentication-rdik.onrender.com/api/user", config)
+  return res.data
+
+
+
+}
+
+const UserDetails = () => {
+  const { data } = useQuery({
+    queryFn: getUserDetails,
+    queryKey: ['user'],
+    suspense: true
+  });
+  return <h3>{`Hello ${data.name}`}</h3>;
+};
 
 
 export const Home = () => {
 
-  const[name,setName]=useState('');
+
 
   const navigate = useNavigate()
 
-  async function getUserDetails(){
-    let config = {
-      headers: {
-        'x-access-token': localStorage.getItem('token'),
-      }
-    }
-    
-      const res= await axios.get("https://node-jwt-authentication-rdik.onrender.com/api/user",config)
-      const data=res.data
-      setName(data.name)
-    
-   
-  }
 
-  useEffect(()=>{
 
-    const token =localStorage.getItem('token')
+  useEffect(() => {
+
+    const token = localStorage.getItem('token')
     const myDecodedToken = decodeToken(token);
 
     if (token) {
       console.log(token);
 
-      if(!myDecodedToken){
-            navigate('/login')
-      }else{
+      if (!myDecodedToken) {
+        navigate('/login')
+      } else {
         getUserDetails()
       }
     }
-    else{
+    else {
       console.log(token)
       navigate('/login')
     }
-  },[])
+  }, [])
 
 
   return (
 
     <>
-    <h3>{`hello ${name}`}</h3>
-    <div className='center'><button onClick={()=>{
-      localStorage.removeItem('token')
-      navigate('/login')
-    }}>logout</button></div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <UserDetails />
+        <div className='center'><button onClick={() => {
+          localStorage.removeItem('token')
+          navigate('/login')
+        }}>logout</button></div>
+      </Suspense>
     </>
   )
 }
